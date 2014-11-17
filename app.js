2d3a87d8fc80cef8ff96121f46f8574238610d17
Francesco Ambrosiano <francescoambrosiano@MacBook-Pro-di-Francesco-Ambrosiano.local>
2014-11-17 17:46:13 +0100

    Time parameter added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("body").randomizza({
-		color: ['#275f9a', '#f15143', '#eebf33', '#d61d49']
+		color: ['#275f9a', '#f15143', '#eebf33', '#d61d49'], 
+		time: 300
 	});
 });
--- js/randomize.js
+++ js/randomize.js
@@ -1,3 +1,6 @@
+// Background randomizer 0.1
+// mail@ottoquattro.com
+
 (function($) {
 
 	'use strict'
@@ -5,30 +8,21 @@
 	$.fn.randomizza = function(options){
 		
 		var settings = $.extend({
-            text         : 'Cacca',
+            time        : 300,
             color        : ['#000', '#333', '#999']
         }, options);
-		// var colors = ['#eee', '#f2f2f2', '#99cc00', '#999', '#cc0011'];
+        
 		var divs = $(".caso");
 		
+		function colori() {
+			var color = settings.color[Math.floor(Math.random()*settings.color.length)];
+			var div = divs[Math.floor(Math.random()*divs.length)];
 
-			function colori() {
-				// var color = colors.sort(function() {return 0.5 - Math.random()});
-				var color = settings.color[Math.floor(Math.random()*settings.color.length)];
-				var divvi = divs.sort(function() {return 0.5 - Math.random()});
-				var div = divs[Math.floor(Math.random()*divs.length)];
+			//Changin' one div background at a time
+			$(div).css("background-color", color);
 
-				//Changin' one div background
-				$(div).css("background-color", color);
-
-				//Changing each div background
-				// $(divvi[0]).css("background-color", colors[0]);
-			 	// 	$(divvi[1]).css("background-color", colors[1]);
-			 	// 	$(divvi[2]).css("background-color", colors[2]);
-			 	// 	$(divvi[3]).css("background-color", colors[3]);
-			 	// 	$(divvi[4]).css("background-color", colors[4]);
-			};
-		 	setInterval(colori, 300); // Time in milliseconds
+		};
+	 	setInterval(colori, settings.time); // Time in milliseconds
 	};
 
 })(jQuery);
