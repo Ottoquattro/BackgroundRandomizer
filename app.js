8c47336503e7a70b1af84d44adc4ec3b081d3618
Francesco Ambrosiano <francescoambrosiano@MacBook-Pro-di-Francesco-Ambrosiano.local>
2014-11-19 17:01:34 +0100

    Images primitive function added - Still no preloader

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$("body").randomizza({
 		color: ['#275f9a', '#f15143', '#eebf33', '#d61d49'], 
-		time: 400
+		time: 300,
+		// images: ['./images/test1.jpg', './images/test2.jpg'] uncomment this to enable images
 	});
 });
